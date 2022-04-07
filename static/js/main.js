9a2260d40e3da4e0caed8082268f1e570bf4f3f7
korayzorlu <koray.zorlu@yahoo.com>
2022-04-07 17:25:11 +0300

    global

--- static/js/main.js
+++ static/js/main.js
@@ -12,44 +12,9 @@ $(document).ready( function () {
             style:    'multi',
             selector: 'td:first-child',
         },
-        buttons: [
-            {
-                text: 'Tümünü Seç',
-                action: function () {
-                    table.rows().select();
-                }
-            },
-            {
-                text: 'Seçimi Kaldır',
-                action: function () {
-                    table.rows().deselect();
-                }
-            }
-        ],
         order: [[ 1, 'asc' ]],
         "bDestroy": true,
         "lengthMenu": [[20, 50, 100, -1], [20, 50, 100, "Tümü"]],
-        /*
-        "processing": true,
-	    "serverSide": true,
-        "paging": true,
-        "pageLength": 20,
-	    "ajax": { 
-		    url: "/user/restapi/products/?format=datatables",
-	    },
-        "columns": [
-            "",
-            { "data": "title", "searchable":true },
-            { "data": "barcode" },
-            { "data": "sku" },
-            { "data": "brand" },
-            { "data": "quantity" },
-            { "data": "sale_price" }
-            { "data": "images", render: function (data, type, row, meta) {
-                return '<img src="' + data + '" height="50" width="50"/>';
-              }}
-        ]
-        */
     } );
     table.on( 'select', function ( e, dt, type, indexes ) {
         document.getElementById("allRemove").style.display='block';
@@ -68,16 +33,6 @@ $(document).ready( function () {
             document.getElementById("allRemove").style.display='none';
         }
     });
-    $('#allRRemove').click( function () {
-        var dataArr = [];
-        $.each($("#dataTable tr.selected"),function(){
-            dataArr.push($(this).find('td').eq(2).text()); 
-        });
-        table.rows('.selected').remove().draw( false );
-        for (let i = 0; i < dataArr.length; i++) {
-            window.location = "/user/deleteproducts/"+dataArr;
-        }
-    } );
     //Remove Rows
     $('#allRemove').click( function () {
         Swal.fire({
